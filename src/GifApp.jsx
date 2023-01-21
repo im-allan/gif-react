d2cbf1a5466ed4554be1a965969743c83aaeecd2
Allan <al.silvaquiroz@gmail.com>
2023-01-20 21:13:02 -0300

    docs: gifapp modified

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
 export const GifApp = () => {
-  return (
+
+    const [categories, setCategories] = useState(['One Piece']);
+    const onAddCategory = ( newCategory ) => {
+        if (categories.map(category => category.toLowerCase()).includes(newCategory.toLowerCase())) return;
+        setCategories([ newCategory , ...categories])
+    }   
+
+
+
+    return (
     <>
     <h1>Gif App</h1>
+    <AddCategory 
+        onNewCategory= { onAddCategory }
+     />
+        { categories.map( category =>(<GifGrid key={ category } category={ category } />))}
     </>
-  )
+    )
 }
